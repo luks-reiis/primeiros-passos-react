f426ab4e4399f35eaf2ec9b086c411c3b3519405
Lucas Reis <luk.wik40@gmail.com>
2023-12-18 23:13:44 -0300

    Aprendendo os tipos de importação e exportação de funções

---
+++ src/app/components/Geo.tsx
@@ -0,0 +1,17 @@
+const Square = () => {
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white">
+            Texto qualquer
+        </div>
+    );
+}
+
+export const Circle = () => {
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white rounded-full">
+            Texto qualquer
+        </div>
+    );
+}
+
+export default Square;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Quadrado, {Circle } from "./components/Geo";
+
 function Page(){
   return (
   <div>
     <h1 className="font-bold text-2xl">Olá Mundo!</h1>
     <h3>Algum texto qualquer</h3>
+    <Quadrado /> 
+    <Circle />
   </div>
   );
 }
