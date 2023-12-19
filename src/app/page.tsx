d1816c48b6ed13d965f03a49c32ec9b293e68c65
Lucas Reis <luk.wik40@gmail.com>
2023-12-18 23:32:17 -0300

    Outras formas de usar componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Quadrado, {Circle } from "./components/Geo";
+import { GeoForm } from "./components/GeoForm";
 
 function Page(){
   return (
   <div>
     <h1 className="font-bold text-2xl">Olá Mundo!</h1>
     <h3>Algum texto qualquer</h3>
-    <Quadrado /> 
-    <Circle />
+    <GeoForm />    
   </div>
   );
 }
