8bfb843bd710d40c5cf10e83e3684f43913d4d9f
Lucas Reis <luk.wik40@gmail.com>
2023-12-19 23:59:17 -0300

    Aprendendo algumas regras do JSX

---
+++ src/app/components/Person.tsx
@@ -0,0 +1,16 @@
+export const Person = () => {
+    return(
+        <>
+            <h1>Elon Musk</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8oc4v5ZVUrnzyc94DOr-V4YvzEHHYLayoYgDPaDCOhPycTWxOi8xHbNobNpWNW-2VHAM" alt="Elon Musk"
+            className = "w-40"
+
+            />
+            <ul>
+                <li>CEO da Tesla</li>
+                <li>CEO da SpaceX</li>
+                <li>CEO da ...</li>
+            </ul>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { GeoForm } from "./components/GeoForm";
+import { Person } from "./components/Person";
 
 function Page(){
   return (
   <div>
     <h1 className="font-bold text-2xl">Olá Mundo!</h1>
     <h3>Algum texto qualquer</h3>
-    <GeoForm />    
+    <Person />
   </div>
   );
 }
