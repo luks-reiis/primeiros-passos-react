6ec794d3271dee9d8295a250e27197dd96e71db1
Lucas Reis <luk.wik40@gmail.com>
2023-12-20 02:28:48 -0300

    Usando objeto e variaveis no componente

--- src/app/components/Person.tsx
+++ src/app/components/Person.tsx
@@ -1,15 +1,35 @@
+const getWeekDay = () =>{
+    return new Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(new Date());
+}
+
 export const Person = () => {
+    const data = {
+        name: 'Elon Musk',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8oc4v5ZVUrnzyc94DOr-V4YvzEHHYLayoYgDPaDCOhPycTWxOi8xHbNobNpWNW-2VHAM',
+        roles: ['CEO da Tesla', 'CEO da SpaceX']
+    }
+
+
+
+
     return(
         <>
-            <h1>Elon Musk</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8oc4v5ZVUrnzyc94DOr-V4YvzEHHYLayoYgDPaDCOhPycTWxOi8xHbNobNpWNW-2VHAM" alt="Elon Musk"
+            <h1 
+            style={{
+                color: 'red', 
+                fontSize: '30px'
+            }}>
+                {data.name} - {getWeekDay()}
+            </h1>
+            <img 
+            src={data.avatar} 
+            alt={data.name}
             className = "w-40"
 
             />
             <ul>
-                <li>CEO da Tesla</li>
-                <li>CEO da SpaceX</li>
-                <li>CEO da ...</li>
+                <li>{data.roles[0]}</li>
+                <li>{data.roles[1]}</li>
             </ul>
         </>
     );
