b9665513d0cd70cba443a54a8db8060098b57e03
Lucas Reis <luk.wik40@gmail.com>
2023-12-25 19:33:56 -0300

    Exemplo de uso do operador && no componente

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,7 +8,9 @@ export const Card = ({ phrase, author }: Props) =>{
             
         <div className="w-96 border-2 border-red-600 p-3 text-3xl text-center italic">
             <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
-            <p className="text-right text-sm">~{author ?? 'Autor Desconhecido'}</p>
+            {author &&
+                <p className="text-right text-sm">~{author}</p>
+            }
         </div>
 
     );
