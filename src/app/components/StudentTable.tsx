72439fc1a8866ed483e39070fffb168c481a8b20
Lucas Reis <luk.wik40@gmail.com>
2023-12-26 04:51:32 -0300

    Exercicio lista de estudantes

---
+++ src/app/components/StudentTable.tsx
@@ -0,0 +1,48 @@
+import { table } from "console";
+import { Student } from "../types/Student";
+
+type Props = {
+    students: Student[];
+}
+
+export const StudentTable = ({ students }: Props) => {
+    return (
+        <table className="w-full border border-gray-600">
+            <thead className="sticky -top-1 z-10 border border-gray-600">
+                <tr className="text-left border-b border-gray-600 bg-gray-800">
+                    <th className="p-3">Nome</th>
+                    <th className="p-3">Status</th>
+                    <th className="p-3">Nota 1</th>
+                    <th className="p-3">Nota 2</th>
+                    <th className="p-3">Nota Final</th>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map(item => (
+                    <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+                        <td className="p-3 flex items-center">
+                            <img src={item.avatar} alt={item.name} className="w-10 h-10 rounded-full mr-3" />
+                            <div>
+                                <div className="font-bold">{item.name}</div>
+                                <div>{item.email}</div>
+                            </div>
+                        </td>
+                        <td className="p-3">
+                            {item.active &&
+                                <div className="px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-xs">Active</div>
+                            }
+                            {!item.active &&
+                                <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-xs">Inactive</div>
+                            }
+                        </td>
+                        <td className="p-3">{item.grade1.toFixed(1)}</td>
+                        <td className="p-3">{item.grade2.toFixed(1)}</td>
+                        <td className="p-3">
+                            {item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--'}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { EmojiRating } from "./components/EmojiRating";
+    import { StudentTable } from "./components/StudentTable";
+    import { students } from "./data/students";
 
-function Page(){
-  return (
-  <div className="w-screen h-screen flex flex-col justify-center items-center">
-    <EmojiRating
-      rate = {5}
-    />
-  </div>
-  );
-}
+    function Page(){
+      return (
+      <div className="container mx-auto pt-5 pb-5">
+        <h1 className="text-5xl mb-5" >Lista de Estudantes</h1>
+        <StudentTable 
+          students = {students}
+        />
+      </div>
+      );
+    }
 
-export default Page;
+    export default Page;
