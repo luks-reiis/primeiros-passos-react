cc40cb2ce1587801b2f18b8d944434682cb1aeab
Lucas Reis <luk.wik40@gmail.com>
2023-12-25 22:40:10 -0300

    Aprendendo a filtrar listas

--- src/app/data/peopleList.ts
+++ src/app/data/peopleList.ts
@@ -4,7 +4,7 @@ export const peopleList: Person[] = [
     { id: 1, name: "Fulano", profession: "matemático" },
     { id: 2, name: "Jão", profession: "químico" },
     { id: 3, name: "Juca", profession: "físico" },
-    { id: 4, name: "Gesabel", profession: "química" },
-    { id: 5, name: "May", profession: "astrofísica" },
+    { id: 4, name: "Gesabel", profession: "químico" },
+    { id: 5, name: "May", profession: "astrofísico" },
     { id: 6, name: "Zezo", profession: "vagabundo" },
   ] 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,22 @@ import { peopleList } from "./data/peopleList";
 
 function Page(){
 
+  const quimicos = peopleList.filter(person => person.profession === "químico");
+
   return (
   <div>
     <h1 className="font-bold text-2xl">Olá Mundo!</h1>
     <h3>Algum texto qualquer</h3>
 
-    {peopleList.length > 0 && 
-      <ul>
-        {peopleList.map((person) => {
-          return <li key={person.id}>{person.name} é {person.profession}</li>}
-        )}
-      </ul>
+    {quimicos.length > 0 &&
+      <>
+        <h3>Lista de químicos:</h3>
+        <ul>
+          {quimicos.map(person => 
+            <li key={person.id}>{person.name}</li>  
+          )}
+        </ul>
+      </>
     }
 
   </div>
