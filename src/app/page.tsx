04c809c05f5a97d8e7ebb46dc1326ee5daa629a7
Lucas Reis <luk.wik40@gmail.com>
2023-12-26 17:56:26 -0300

    Modo de enviar paramêtros em eventos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client"
 
 function Page(){
-  function handleClick(){
-    alert("Funcionou");
+  function exibirMsg(msg: string){
+    alert(msg)
   }
 
 
   return (
-  <div className="w-screen h-screen flex justify-center items-center">
-    <button onClick={handleClick} className="p-3 bg-blue-700 text-white rounded-md">Clique aqui</button>
+  <div className="w-screen h-screen flex justify-center items-center gap-10">
+    <button onClick={() => exibirMsg('mensagem 1')} className="p-3 bg-blue-700 text-white rounded-md">Clique aqui 1</button>
+    <button onClick={() => exibirMsg('mensagem 2')} className="p-3 bg-blue-700 text-white rounded-md">Clique aqui 2</button>
   </div>
   );
 }
