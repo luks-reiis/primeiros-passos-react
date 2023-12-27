aad795dd6ccfa850c05d720df3dbffe76b0cb6d5
Lucas Reis <luk.wik40@gmail.com>
2023-12-27 02:50:56 -0300

    Aprendendo a usar states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 "use client"
 
-import { FormEvent } from "react";
-import { CustomButton } from "./components/CustomButton";
+import { useState } from "react";
 
 function Page(){
+  const [count, setCount] = useState(0); 
 
-  const handleFormSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    alert("Opa, enviando...");
+  const handleClickButton = () => {
+    setCount(count + 1);
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl mb-3">Form de Login</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input className="text-black" type="text" />
-        <input type="submit" value="Enviar" />
-      </form>
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <p>{count}</p>
+      <button className="bg-blue-500 p-3" onClick={handleClickButton}>+1</button>
     </div>
     );
 }
