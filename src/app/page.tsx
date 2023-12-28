a9fb0e248186b04e0020914fb9c5d1e2338c845c
Lucas Reis <luk.wik40@gmail.com>
2023-12-27 22:00:58 -0300

    Renderização dinâmica de um input com states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,27 @@
 import { useState } from "react";
 
 function Page(){
-  const [showSecret, setShowSecret] = useState(false);
 
-  const handleClickButton = () => {
-    setShowSecret(!showSecret);
+  const [nameInput, setNameInput] = useState('');
+
+  const handleBtnClick = () => {
+
+    alert(nameInput)
+
   }
+
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <button className="bg-blue-500 p-3" onClick={handleClickButton}>{showSecret ? 'Ocultar' : 'Mostrar'}</button>
-      {showSecret &&
-        <div className="p-3 bg-blue-300 rounded-md mt-3">Área Secreta</div>
-      }
+      <input 
+        className="border border-black p-3 text-xl text-black rounded" 
+        type="text" 
+        placeholder="Digite seu nome"
+        value={nameInput}
+        onChange={e => setNameInput(e.target.value)}
+      />
+      <p>Seu nome é: {nameInput}</p>
+      <button onClick={handleBtnClick}>Mostrar valor do campo</button>
     </div>
     );
 }
