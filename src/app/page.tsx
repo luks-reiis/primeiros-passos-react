c2120291426016a77338817afe8c1304442840db
Lucas Reis <luk.wik40@gmail.com>
2024-01-04 01:53:44 -0300

    Criadas funções de um CRUD de uma lista antes de um reducer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,51 @@
 "use client"
 
 import { use, useEffect, useState } from "react";
-import { Square } from "./components/Square";
+import { Item } from "./types/Item";
 
 function Page(){
 
-  const[show, setShow] = useState(false);
+  const [list, setList] = useState<Item[]>([]);
+
+  const addNewItem = (text: string) => {
+    setList([ ...list, {
+      id: list.length, 
+      text, 
+      done:false
+    }])
+  }
+
+  const editItemText = (id: number, newText: string) => {
+    setList(
+      list.map(t => {
+        if(t.id === id){
+          t.text = newText;
+        }
+        return t;
+      })
+    )
+  }
+
+  const toggleItem = (id: number) => {
+    setList(
+      list.map(t => {
+        if(t.id === id){
+          t.done = !t.done;
+        }
+        return t;
+      })
+    )
+  }
+
+  const removeItem = (id: number) => {
+    setList(list.filter(t => t.id !== id));
+  }
 
   return (
     <div className="">
-      <button onClick={() => setShow(!show)}>Mostrar/Ocultar</button>
 
-      {show && <Square/>}
     </div>
-    );
+    );erfdwefcwerd
 }
 
 export default Page;
